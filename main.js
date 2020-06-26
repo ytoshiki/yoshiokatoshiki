e845ce3d9430f358bfd7f35fbb6d39d8f513a08b
Toshiki Yoshioka <60994269+ytoshiki@users.noreply.github.com>
2020-06-26 19:07:03 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,21 @@
-const buttons = document.querySelector('.categories').children;
+const buttons = document.querySelector(".categories").children
 
-const project = document.querySelector('.projects').children;
+const project = document.querySelector(".projects").children
 
-const projectWrapper = document.querySelector('.projects');
-
+const projectWrapper = document.querySelector(".projects")
 
 for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener('click', (e)=> {
-
-    const x = e.target.getAttribute('data-target');
+  buttons[i].addEventListener("click", e => {
+    const x = e.target.getAttribute("data-target")
 
     for (let k = 0; k < project.length; k++) {
+      project[k].style.display = "none"
 
-      project[k].style.display = "none";
-
-      if (project[k].getAttribute('data-id') === x) {
-
-        project[k].style.display = '';
-
+      if (project[k].getAttribute("data-id") === x) {
+        project[k].style.display = ""
       }
 
-      if (x == 'all') project[k].style.display = '';
+      if (x == "all") project[k].style.display = ""
     }
   })
-    
 }
